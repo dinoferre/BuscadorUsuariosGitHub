104e816f27cdc8f9bd54b912aba01920167fad9e
Dino Ferré <dinoferre@hotmail.com>
2024-05-09 17:05:12 -0300

    update favoritos

--- js/main.js
+++ js/main.js
@@ -6,15 +6,29 @@ const app = Vue.createApp({
             search: null, // Variable para almacenar la búsqueda del usuario
             result: null, // Variable para almacenar los datos del usuario encontrado
             error: null, // Variable para almacenar cualquier error ocurrido durante la búsqueda
-            favorites: new Map()
+            favorites: new Map() // Mapa para almacenar los usuarios favoritos
         };
+    },
+    // Método creado cuando se instancia la aplicación Vue
+    created(){
+        const savedFavorites = JSON.parse(window.localStorage.getItem("favorites"))
+        if(savedFavorites?.length){
+            const favorites = new Map(savedFavorites.map(favorite => [favorite.id, favorite]))
+            this.favorites = favorites
+        }
     },
     computed: {
         isFavorite(){
+            // Computed property para verificar si el usuario actual es favorito
             return this.favorites.has(this.result.id)
+        },
+        allFavorites(){
+            // Computed property para obtener todos los favoritos como un array
+            return Array.from(this.favorites.values())
         }
     },
     methods: {
+        // Método para realizar la búsqueda de usuario en GitHub
         async doSearch() {
             this.result = this.error = null // Reinicia los resultados y errores en cada nueva búsqueda
 
@@ -33,11 +47,25 @@ const app = Vue.createApp({
         },
 
         addFavorite(){
+            // Método para agregar un usuario a la lista de favoritos
             this.favorites.set(this.result.id, this.result)
+            this.updateStorage()
         },
 
         removeFavorite(){
+            // Método para eliminar un usuario de la lista de favoritos
             this.favorites.delete(this.result.id)
+            this.updateStorage()
+        },
+
+        showFavorite(favorite){
+            // Método para mostrar los detalles de un usuario favorito
+            this.result = favorite
+        },
+
+        updateStorage(){
+            // Método para actualizar el almacenamiento local con la lista de favoritos
+            window.localStorage.setItem("favorites", JSON.stringify(this.allFavorites))
         }
     }
 })
