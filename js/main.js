a4dcb41ed1e101f604bb5b5048679bec08bff894
Dino Ferré <dinoferre@hotmail.com>
2024-06-25 14:05:30 -0300

    soluciono error de if-else

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const API = "https://api.github.com/users/";
-
-const requestMaxTimeMs = 3000
+const requestMaxTimeMs = 3000;
 
 const app = Vue.createApp({
   data() {
@@ -12,71 +11,69 @@ const app = Vue.createApp({
     };
   },
   created() {
-    const savedFavorites = JSON.parse(window.localStorage.getItem("favorites"))
+    const savedFavorites = JSON.parse(window.localStorage.getItem("favorites"));
     if (savedFavorites?.length) {
-      const favorites = new Map(savedFavorites.map(favorite => [favorite.login, favorite]))
-      this.favorites = favorites
+      const favorites = new Map(savedFavorites.map(favorite => [favorite.login, favorite]));
+      this.favorites = favorites;
     }
   },
   computed: {
     isFavorite() {
-      return this.favorites.has(this.result.login)
+      return this.favorites.has(this.result?.login);
     },
     allFavorites() {
-      return Array.from(this.favorites.values())
+      return Array.from(this.favorites.values());
     }
   },
   methods: {
     async doSearch() {
-      this.result = this.error = null
+      this.result = this.error = null;
 
-      const foundInFavorites = this.favorites.get(this.search)
+      const foundInFavorites = this.favorites.get(this.search);
 
       const shouldRequestAgain = (() => {
-        if (!!foundInFavorites) {
-          const { lastRequestTime } = foundInFavorites
-          const now = Date.now()
-          return (now - lastRequestTime) > requestMaxTimeMs
+        if (foundInFavorites) {
+          const { lastRequestTime } = foundInFavorites;
+          const now = Date.now();
+          return (now - lastRequestTime) > requestMaxTimeMs;
         }
-        return false
-      })() // IIFE
+        return false;
+      })();
 
-      if (!!foundInFavorites && !shouldRequestAgain) {
-        return this.result = foundInFavorites
+      if (foundInFavorites && !shouldRequestAgain) {
+        return this.result = foundInFavorites;
       }
 
       try {
-        const response = await fetch(API + this.search)
-        if (!response.ok) throw new Error("User not found")
-        const data = await response.json()
-        this.result = data
-        foundInFavorites.lastRequestTime = Date.now()
-      } catch(error) {
-        this.error = error
+        const response = await fetch(API + this.search);
+        if (!response.ok) throw new Error("User not found");
+        const data = await response.json();
+        this.result = data;
+        this.result.lastRequestTime = Date.now();
+      } catch (error) {
+        this.error = error;
       } finally {
-        this.search = null
+        this.search = null;
       }
     },
 
     addFavorite() {
-      this.result.lastRequestTime = Date.now()
-      this.favorites.set(this.result.login, this.result)
-      this.updateStorage()
+      this.result.lastRequestTime = Date.now();
+      this.favorites.set(this.result.login, this.result);
+      this.updateStorage();
     },
 
     removeFavorite() {
-      this.favorites.delete(this.result.login)
-      this.updateStorage()
+      this.favorites.delete(this.result.login);
+      this.updateStorage();
     },
 
     showFavorite(favorite) {
-      this.result = favorite
+      this.result = favorite;
     },
 
     updateStorage() {
-      window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites))
+      window.localStorage.setItem("favorites", JSON.stringify(this.allFavorites));
     }
   }
 });
-
-const mountedApp = app.mount("#app")
